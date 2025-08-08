b6fa7301e7bafe0cc51e057107a0cfa5c5e6073d
FRKenzoYTB <kenzo0025@outlook.fr>
2025-08-08 12:53:11 +0200

    Create login.js

---
+++ login.js
@@ -0,0 +1,41 @@
+// login.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAzSzAc3Fz18PdgumpGY3_s2K42v2MzYK0",
+  authDomain: "message-957c6.firebaseapp.com",
+  databaseURL: "https://message-957c6-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "message-957c6",
+  storageBucket: "message-957c6.firebasestorage.app",
+  messagingSenderId: "518720941529",
+  appId: "1:518720941529:web:7c6b444ef9ac0b0d127dc8",
+  measurementId: "G-GW0GKGQ9EP"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const btn = document.getElementById("loginBtn");
+btn.addEventListener("click", () => {
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value;
+
+  if (!email || !password) {
+    alert("Tous les champs sont requis !");
+    return;
+  }
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      window.location.href = "home.html";
+    })
+    .catch((error) => {
+      alert("Erreur : " + error.message);
+    });
+});
+
+// Si utilisateur déjà connecté, redirige vers home
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    window.location.href = "
