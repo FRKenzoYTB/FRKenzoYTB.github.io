a4b05cd5ca859354871502ac02781ea480e7094e
FRKenzoYTB <kenzo0025@outlook.fr>
2025-08-10 12:12:18 +0200

    Update home.js

--- home.js
+++ home.js
@@ -1,1 +1,22 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
+import { getAuth, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
+import {
+  getDatabase,
+  ref,
+  get,
+  set,
+  push,
+  onValue,
+  update,
+  remove,
+} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-database.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAzSzAc3Fz18PdgumpGY3_s2K42v2MzYK0",
+  authDomain: "message-957c6.firebaseapp.com",
+  databaseURL:
+    "https://message-957c6-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "message-957c6",
+  storageBucket: "message-957c6.firebasestorage.app",
+  messagingSenderId: "518720941529",
+  appId: "1:518720941529:web:7c6b444ef9ac0b0d
